4340c881632932dd58d502b4dd11cb2dc57d12e4
Drabis <drabis47@gmail.com>
2021-05-08 14:40:07 -0400

    render the homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,25 +1,11 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Service } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
 
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('main', {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get("/", (req, res) => {
+  
+  res.render('homepage')
 });
 
 router.get('/login', (req, res) => {
