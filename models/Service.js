a33297546fb831f1cc6f81e1af0479b3c9a86766
Drabis <drabis47@gmail.com>
2021-05-08 13:17:30 -0400

    created user and service models

---
+++ models/Service.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class Service extends Model {}
+
+Service.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    points: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "service",
+  }
+);
+
+module.exports = Service;
