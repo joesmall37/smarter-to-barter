014a24ba61ab2e4afc5108a91b09bcc9e1625c18
Joe Small <small.joseph@outlook.com>
2021-05-11 19:45:29 -0400

    new post req

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,6 +29,76 @@ router.get("/", (req, res) => {
 //     console.log(err);
 //     res.status(500).json(err);
 //   });
+// Use withAuth middleware to prevent access to route
+router.get('/userprofile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('userprofile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+router.post('/signup', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+
+router.post('/profile', async (req, res) => {
+  try {
+    const userData = await Service.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+
+
+//     // Serialize data so the template can read it
+//     const projects = projectData.map((project) => project.get({ plain: true }));
+
+//     // Pass serialized data and session flag into template
+//     res.render('homepage', {
+//       projects,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 
 
@@ -76,6 +146,7 @@ router.get('/signup', (req, res) => {
 
   res.render('signup');
 });
+
 
 
 
---
+++ models/Provider.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class Provider extends Model { }
+
+Provider.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        points: {
+            allowNull: false,
+            type: DataTypes.INTEGER,
+        },
+        service_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Service',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "service",
+    }
+);
+
+module.exports = Provider;
